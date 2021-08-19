489f2352d75c1493f6188b1db3be2b9714958759
Apthox <kevin@apthos.net>
2021-08-19 02:30:44 -0700

    structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react"
-import Button1 from "./Button1"
-import Button2 from "./Button2"
-import Button3 from "./Button3"
-import Button4 from "./Button4"
+import Button1 from "./button1"
+import Button2 from "./button2"
+import Button3 from "./button3"
+import Button4 from "./button4"
  
 
 const App = () => {
