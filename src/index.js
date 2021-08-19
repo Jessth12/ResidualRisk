c60cb1635c08354a6049af023b5255973a4067d3
Yessica <jessh2182@gmail.com>
2021-08-18 20:54:04 -0700

    structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./components/App"
+import "../src/index.css"
  
 const reactContentRoot = document.getElementById("root")
 
